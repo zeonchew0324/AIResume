b17d74afb647cfb65f2e160f058b14c0488aee3c
Chew Tsze Honn <zeonchew0324@gmail.com>
2025-06-21 01:12:56 +0800

    Create title template

--- AiResume/main.py
+++ AiResume/main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from airesume!")
+import streamlit as st
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+import io
+import PyPDF2
 
+load_dotenv()
 
-if __name__ == "__main__":
-    main()
+open_api_key = os.getenv("OPENAI_API_KEY")
+
+st.set_page_config(page_title="AI Resume Analyzer", page_icon=":guardsman:", layout="centered")
+
+st.title("AI Resume Analyzer")
+
+st.markdown("Welcome to the AI Resume Analyzer! Upload your resume in PDF format, and enjoy your feedback!")
